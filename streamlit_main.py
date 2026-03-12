5c0ad1adb3bcdedc1f69780a47edc255206afdbc
bartimaeus50 <kmy5678@gmail.com>
2026-03-12 21:20:15 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,7 +1,37 @@
 import streamlit as st
 import joblib
 
-model = joblib.load("freq.pkl")
+# 모델 로딩
+clf = joblib.load("freq.pkl")
+
+def detect_lang(text):
+    # 알파벳 출현빈도 구하기
+    text = text.lower()
+
+    cnt = []
+    for i in range(26):
+        ch = chr(i + ord('a'))
+        cnt.append(text.count(ch))
+
+    total = sum(cnt)
+    if total == 0:
+        return "입력이 없습니다"
+
+    # 입력데이터 준비 (전처리)
+    freq = list(map(lambda n: n / total, cnt))
+
+    # 예측
+    pred = clf.predict([freq])
+
+    # 코드 → 언어명 변환
+    lang_dic = {
+        "en": "영어",
+        "fr": "프랑스어",
+        "id": "인도네시아어",
+        "tl": "타갈로그어"
+    }
+
+    return lang_dic[pred[0]]
 
 st.title("외국어 문장 판별")
 st.write("외국어 입력 (입력후 CTRL + ENTER)")
@@ -9,5 +39,4 @@ st.write("외국어 입력 (입력후 CTRL + ENTER)")
 text = st.text_area("외국어 문장 입력", label_visibility="collapsed")
 
 if text:
-    pred = model.predict([text])[0]
-    st.write(pred)
+    st.write(detect_lang(text))
