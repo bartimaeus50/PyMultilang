7c1311c13db78135b560061db6af8c3790fccc52
bartimaeus50 <kmy5678@gmail.com>
2026-03-12 21:12:29 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,21 +1,13 @@
 import streamlit as st
 import joblib
 
-# 모델 불러오기
 model = joblib.load("freq.pkl")
 
-# 제목
 st.title("외국어 문장 판별")
-
-# 입력 안내
 st.write("외국어 입력 (입력후 CTRL + ENTER)")
 
-# 문장 입력창
 text = st.text_area("외국어 문장 입력", label_visibility="collapsed")
 
-# 입력이 있으면 자동 분석
 if text:
-    X = vectorizer.transform([text])
-    pred = model.predict(X)[0]
-
-    st.write(pred)
+    pred = model.predict([text])[0]
+    st.write(pred))
